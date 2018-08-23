6c656141e734a8983c6c63eadd59047566eb2452
sshong1991 <sshong1991@gmail.com>
2018-08-23 23:49:52 +0900

    image create

--- orly/cover/views.py
+++ orly/cover/views.py
@@ -7,7 +7,7 @@ def index(request):
     if request.method == 'POST':
         form = CoverForm(request.POST)
         if form.is_valid():
-            form.cleaned_data
+            form.cleaned_data # 이미지를 생성할 데이터가 있는곳
             return redirect('cover:index')
     else:
         form = CoverForm
@@ -15,8 +15,9 @@ def index(request):
         'form': form,
     })
 
+# 위 데이터를 받아서 이미지는 여기에서 그린다
 def image_generator(request):
     im = Image.new('RGB', (256, 256))
-    response = HttpResponse()
+    response = HttpResponse(content_type='image/jpeg')
     im.save(response, format='JPEG')
-    return res ponse
+    return response
