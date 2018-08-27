98713e97f14937f7b597cb27fe4e27d147c4afe2
sshong1991 <sshong1991@gmail.com>
2018-08-27 17:07:36 +0900

    animals image add & color code

--- orly/cover/views.py
+++ orly/cover/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from PIL import Image, ImageFont, ImageDraw
 from .forms import CoverForm
+from .utils import COLOR_CODES
 
 
 def index(request):
@@ -22,21 +23,42 @@ def image_generator(request):
     title = request.GET['title']
     top_text = request.GET['top_text']
     author = request.GET['author']
+    animal_code = request.GET['animal_code']
+    color_index = request.GET['color_code']
+    guide_text = request.GET['guide_text']
+    guide_text_placement = request.GET['guide_text_placement']
 
-    im = Image.new('RGB', (256, 256))
+    animal_path = settings.ROOT('assets', 'animals', '{}.png'.format(animal_code))
+    animal_im = Image.open(animal_path)
+
+    color = COLOR_CODES[int(color_index)]
+
+    canvas_im = Image.new('RGB', (500, 700), (255, 255, 255, 255))
+
+    animal_im = animal_im.resize((400, 400))
+    canvas_im.paste(animal_im, (50, 40))
 
     ttf_path = settings.ROOT('assets', 'fonts', 'NanumGothicCoding.ttf')
-    d = ImageDraw.Draw(im)
+    draw = ImageDraw.Draw(canvas_im)
 
-    fnt = ImageFont.truetype(ttf_path, 40)
-    d.text((10, 10), title, font=fnt, fill=(0, 255, 0, 128))
+    draw.rectangle((20, 0, 480, 10), fill=color)
+
+    draw.rectangle((10, 400, 400, 510), fill=color)
+
+    fnt = ImageFont.truetype(ttf_path, 70)
+    draw.text((45, 430), title, font=fnt, fill=(255, 255, 255, 255))
 
     fnt = ImageFont.truetype(ttf_path, 20)
-    d.text((10, 60), top_text, font=fnt, fill=(0, 255, 0, 255))
+    draw.text((160, 15), top_text, font=fnt, fill=(0, 0, 0, 255))
 
-    fnt = ImageFont.truetype(ttf_path, 10)
-    d.text((10, 110), author, font=fnt, fill=(0, 255, 0, 255))
+    fnt = ImageFont.truetype(ttf_path, 25)
+    draw.text((360, 655), author, font=fnt, fill=(0, 0, 0, 255))
+
+    fnt = ImageFont.truetype(ttf_path, 30)
+    position = (125, 505)
+    draw.text(position, guide_text, font=fnt, fill=(0, 0, 0, 255))
+
 
     response = HttpResponse(content_type='image/png')
-    im.save(response, format='PNG')
+    canvas_im.save(response, format='PNG')
     return response
